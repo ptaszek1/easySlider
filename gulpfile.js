d6f727a38e0490694f1aa989c21a483772af9593
Ptaszek1 <mariusz.ptaszek2@gmail.com>
2020-02-27 18:18:36 +0100

    Created simple slider

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+const gulp            = require('gulp');
+const rename          = require('gulp-rename');
+const uglify          = require('gulp-uglify');
+const sass            = require('gulp-sass');
+const autoprefixer    = require('gulp-autoprefixer');
+const plumber         = require('gulp-plumber');
+const sourcemaps      = require('gulp-sourcemaps');
+const colors          = require('ansi-colors');
+const wait            = require('gulp-wait');
+const notify          = require("gulp-notify");
+const babel           = require("gulp-babel");
+const concat          = require("gulp-concat");
+const browserify      = require('browserify');
+const babelify        = require('babelify');
+const watchify        = require('watchify');
+const buffer          = require('vinyl-buffer');
+const source          = require('vinyl-source-stream');
+
+function showError(err) {
+
+    notify.onError({
+        title: "Gulp error in " + err.plugin,
+        message:  err.message
+    })(err);
+
+    console.log(colors.red(err.toString())  );
+    this.emit('end');
+}
+
+function compile(watch) {
+    var bundler = watchify(browserify('./src/js/app.js', {debug: true}).transform(babelify, {
+        // Use all of the ES2015 spec
+        presets: ["@babel/env"],
+        sourceMaps: true
+    }));
+
+    function rebundle() {
+        return bundler
+            .bundle()
+            .on('error', function (err) {
+                console.error(err);
+                this.emit('end');
+            })
+            .pipe(source('src/js/app.js'))
+            .pipe(buffer())
+            .pipe(rename('app.min.js'))
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('src/js'));
+    }
+
+    if (watch) {
+        bundler.on('update', function () {
+            console.log('-> bundling...');
+            rebundle();
+        });
+
+        rebundle();
+    } else {
+        rebundle().pipe(exit());
+    }
+}
+
+gulp.task('sass', function () {
+    console.log('compiling css');
+    return gulp.src('./src/sass/main.scss')
+        .pipe(wait(500))
+        .pipe(plumber({
+            errorHandler : showError
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle : 'compressed'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./src/css'));
+});
+
+function watch() {
+    return compile(true);
+}
+
+gulp.task('build', function () {
+    return compile();
+});
+gulp.task('watch', function () {
+    return watch();
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    return watch();
+    // gulp.watch('./js/*.js', ['js']);
+});
+
+gulp.task('default', function () {
+    console.log(colors.bold(colors.yellow('----- rozpoczyam pracę ------')));
+    gulp.start(['watch']);
+});
---
+++ src/js/app.js
@@ -0,0 +1,164 @@
+class clearSlider {
+    constructor(domElement,config = {}) {
+        this.main = domElement;
+        this.random = false;
+        this.autoplay = false;
+        this.navigation = false;
+        this.next;
+        this.prev;
+        this.customWrapper = '';
+        this.speed = 400;
+        this.activeSlide = 0;
+        this.slidesPerView = 1;
+        this.sizes;
+        this.autoplaySpeed;
+        this.autoplayDirection;
+        Object.assign(this,config);
+        this.init();
+        this.setEventResize();
+        this.setEventClick();
+    }
+    setEventClick() {
+        this.next.addEventListener("click", this.nextSlide.bind(this));
+        this.prev.addEventListener("click", this.prevSlide.bind(this));
+    }
+    setEventResize(){
+        window.addEventListener("resize", this.setSize.bind(this));
+    }
+    init() {
+        this.slides = this.getSlides();
+        this.wrapper = this.getWrapper();
+        this.parent = this.getParent();
+        this.sizes = this.slidesSize();
+        this.randomValue = this.randomSlide();
+        // this.error = this.error();
+        this.setSize();
+        this.setSlideIndex();
+        this.setActiveSlide();
+        this.getNavigation();
+        this.runAutoplay();
+    }
+    getParent() {
+        return (typeof this.main === 'string' && this.main.length > 1) ? document.querySelector(this.main): '';
+    }
+    getSlides() {
+        if(this.main) {
+            return document.querySelectorAll(this.main + ' .es-slide');
+        }
+    }
+    getWrapper() {
+        if(this.main) {
+            return document.querySelector(this.main + ' .es-wrapper');
+        }
+    }
+    slidesSize() {
+        return {
+            width: this.parent.offsetWidth,
+            height: this.parent.offsetHeight
+        }
+    }
+    setSize() {
+        if(this.slides.length > 0) {
+            for(let i = 0; i < this.slides.length; i++) {
+                if(this.slidesPerView > 1 && this.slidesPerView <= this.slides.length) {
+                    this.slides[i].style.width = (this.sizes.width / this.slidesPerView) + 'px';
+                } else {
+                    this.slides[i].style.width = this.sizes.width + 'px';
+                }
+            }
+        }
+    }
+    setSlideIndex() {
+        if(this.slides.length > 0) {
+            for(let i = 0; i < this.slides.length; i++) {
+                this.slides[i].dataset.index = i +1;
+            }
+        }
+    }
+    setActiveSlide(){
+        let activeSlideIndex = this.activeSlide -1;
+        // If random is set to true.
+        if(this.random === true) {
+            activeSlideIndex = this.randomValue -1;
+        }
+        if(this.slides.length >= this.slidesPerView && activeSlideIndex >= 0 && this.activeSlide <= this.slides.length) {
+            // Set active slide
+            this.slides[activeSlideIndex].classList.add('es-active-slide');
+            if(this.slidesPerView > 1) {
+                console.log(-this.sizes.width * activeSlideIndex + this.sizes.width)
+                this.wrapper.style.transform = `translate3d(${((-this.sizes.width * activeSlideIndex) / this.slidesPerView)}px, 0px, 0px)`;  
+            } else {
+                this.wrapper.style.transform = `translate3d(-${this.sizes.width * (activeSlideIndex)}px, 0px, 0px)`;
+            }
+            setTimeout(()=> {
+                this.wrapper.style.transitionDuration = this.speed + 'ms';
+            },1)
+            // Set previous slide
+            if(this.slides[activeSlideIndex].previousElementSibling !== null) {
+                this.slides[activeSlideIndex].previousElementSibling.classList.add('es-previous-slide');
+            } else if(this.slides.length > 2) {
+                this.slides[this.slides.length-1].classList.add('es-previous-slide');
+            }
+            // Set next slide
+            if(this.slides[activeSlideIndex].nextElementSibling !== null) {
+                this.slides[activeSlideIndex].nextElementSibling.classList.add('es-next-slide');
+            } else if(this.slides.length > 2) {
+                this.slides[0].classList.add('es-next-slide');
+            }
+        } else {
+            console.log('błąd')
+        }
+    }
+    randomSlide() {
+        return Math.floor(Math.random() * (this.slides.length - 1 + 1)) + 1;
+    }
+    getNavigation() {
+        if(this.navigation === true) {
+            const navigationWrapper = document.querySelector('.es-navigation');
+            if(navigationWrapper) {
+                this.next = navigationWrapper.querySelector('.es-navigation-right');
+                this.prev = navigationWrapper.querySelector('.es-navigation-left');
+            }
+        }
+    }
+    nextSlide() {
+        this.activeSlide++;
+        this.setActiveSlide();
+    }
+    prevSlide() {
+        this.activeSlide--;
+        this.setActiveSlide();
+    }
+    runAutoplay() {
+        if(this.autoplay === true) {
+            if(this.autoplayDirection === 'left') {
+                setInterval(() => {
+                    this.activeSlide--;
+                    this.setActiveSlide();
+                }, this.autoplaySpeed);
+            } else {
+                setInterval(() => {
+                    this.activeSlide++;
+                    this.setActiveSlide();
+                }, this.autoplaySpeed);
+            }
+        }
+    }
+}
+
+
+
+
+// Zrobić wyłączanie navigacji jak dojedzie do końca
+
+const simpleSlider = new clearSlider('.es-container',{
+    speed: 300,
+    slidesPerView: 2,
+    activeSlide: 1,
+    navigation: true,
+    autoplay: false,
+    autoplaySpeed: 1200,
+    autoplayDirection: 'right'
+});
+
+console.log(simpleSlider);
